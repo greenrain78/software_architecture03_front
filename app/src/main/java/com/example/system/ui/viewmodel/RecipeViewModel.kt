8ad2e89ff20bfed3d54cc0e1749b4a342e5819c3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-12-08 23:29:55 +0900

    feat : Create RecipeViewModel

---
+++ app/src/main/java/com/example/system/ui/viewmodel/RecipeViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.system.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.system.data.repository.RecipeRepository
+import com.example.system.ingredientsDB.Ingredient
+import com.example.system.ingredientsDB.IngredientDao
+import com.example.system.ingredientsDB.IngredientRepository
+import kotlinx.coroutines.launch
+
+class RecipeViewModel : ViewModel() {
+
+    private val recipeRepository = RecipeRepository()
+
+    fun getRecipes() =
+        viewModelScope.launch {
+            recipeRepository.getRecipes()
+        }
+
+    fun postRecipe(ingredients: List<Ingredient>, name: String, description: String) =
+        viewModelScope.launch {
+            recipeRepository.registerRecipe(ingredients, name, description)
+        }
+
+    fun modifyRecipe(ingredients: List<Ingredient>, id: Int, name: String, description: String) =
+        viewModelScope.launch {
+            recipeRepository.putRecipe(ingredients, id, name, description)
+        }
+
+}
