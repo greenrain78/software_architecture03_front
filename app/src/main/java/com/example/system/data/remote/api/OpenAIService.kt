5f33d8c5fe60524c05f25b487a528ffd2087edf2
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2024-12-17 23:30:23 +0900

    feat: get vision

--- app/src/main/java/com/example/system/data/remote/api/OpenAIService.kt
+++ app/src/main/java/com/example/system/data/remote/api/OpenAIService.kt
@@ -1,5 +1,12 @@
 package com.example.system.data.remote.api
 
-interface OpenAIService {
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
 
+interface OpenAIService {
+    @POST("chat/completions") // API 엔드포인트에 맞게 설정
+    suspend fun generateImageFromUrl(
+        @Body request: ChatRequest
+    ): Response<GPTResponse>
 }
